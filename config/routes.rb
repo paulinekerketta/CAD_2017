3ee6ccc21a4270339ee019157b7a032f3efd6a0c
paulinekerketta <psk_011@yahoo.com>
2017-07-31 11:03:00 -0700

    adding login in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   get 'welcome/contactus'
 
   get 'welcome/aboutus'
+
+  get 'welcome/login'
+  
 root to: 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
